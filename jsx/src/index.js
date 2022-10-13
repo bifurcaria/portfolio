72fca5cf357900332c3dbbcd9c18dcee4ee53559
bifurcaria <c.carlajm@gmail.com>
2022-10-13 17:31:45 -0300

    delete folders

---
+++ jsx/src/index.js
@@ -0,0 +1,10 @@
+// 1. importar react y librerias de reactDOM
+
+// 2. Referirse al div con ID root
+
+// 3. Decirle a React que tome control del elemento
+
+// 4. Crear un componente
+
+// 5. Mostrar el componente en la pantalla
+

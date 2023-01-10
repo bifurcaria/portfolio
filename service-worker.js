27619f3e243bf9b85b9ac40b8ed7ee67169ee5a8
bifurcaria <c.carlajm@gmail.com>
2023-01-10 17:59:18 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7bc8086e401a62814640d2b6f2599012.js"
+  "/portfolio/precache-manifest.3f7bc066fb17711672d4e7f01f6e4f5e.js"
 );
 
 workbox.clientsClaim();
